e3494612563a9b99975c7cfb67c0c2e211f513fa
wonjw <jwwon00@naver.com>
2025-05-01 12:12:14 +0900

    임시저장

--- Source/Statistic/MyFireWeapon.cpp
+++ Source/Statistic/MyFireWeapon.cpp
@@ -8,3 +8,20 @@ AMyFireWeapon::AMyFireWeapon()
 	WeaponType = EWeaponType::WT_Fire;
 	BaseSocketName = TEXT("FirePosition");
 }
+
+void AMyFireWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMyFireWeapon::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AMyFireWeapon::SpawnFireBall()
+{
+	//TempEnergyBall = Cast<AFireBall>(GetWorld()->SpawnActor(EnergyBallClass));
+	UE_LOG(LogTemp, Warning, TEXT("FireBall Spawned"));
+	
+}
